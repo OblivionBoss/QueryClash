1092fb0d635edb1a3829854331ad036e0578be93
OblivionBoss <90751468+OblivionBoss@users.noreply.github.com>
2024-09-29 18:49:05 +0700

    Add MainMenu and GameLobby

---
+++ QueryClash/Assets/Script/GameLobby/GetOppIP.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using FishNet.Transporting.Tugboat;
+using FishNet;
+
+public class GetOppIP : MonoBehaviour
+{
+    public static GetOppIP getOppIP;
+    public TMP_InputField inputField;
+    public string Opp_IP;
+    public Tugboat TG;
+
+    private void Awake() {
+        if (getOppIP == null) { getOppIP = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); }
+    }
+
+    public void GetIPv4() { 
+        Opp_IP = inputField.text;
+        //TG.SetClientAddress(Opp_IP);
+        //InstanceFinder.NetworkManager.GetComponent<Tugboat>().SetClientAddress(Opp_IP);
+        //InstanceFinder.NetworkManager.GetComponent<Tugboat>().SetClientAddress();
+        SceneManager.LoadSceneAsync("Game"); 
+    }
+}
