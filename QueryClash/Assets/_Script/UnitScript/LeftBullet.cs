071757cf012f15b5f0be0ec9c5c964bbb6984f3c
Thirachai Ngaoju <gxz.gunforce@gmail.com>
2024-12-16 15:23:40 +0700

    "Edit click other summon button before place current summoned unit"

--- QueryClash/Assets/_Script/UnitScript/LeftBullet.cs
+++ QueryClash/Assets/_Script/UnitScript/LeftBullet.cs
@@ -1,5 +1,3 @@
-using FishNet.Object;
-using FishNet.Object.Synchronizing;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +8,8 @@ public class LeftBullet : MonoBehaviour
     public float deadZone;
     public float Atk = 10;
 
-    void Update() // Update positions of certain bullets
-    {
+    void Update()
+    {   
         transform.position = transform.position + (Vector3.right * bulletspeed) * Time.deltaTime;
         if (transform.position.x > deadZone)
         {
@@ -20,7 +18,7 @@ public class LeftBullet : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider collision) // Check when bullet hit a target and check if target is an enemy or not
+    private void OnTriggerEnter(Collider collision)
     {
 
         Soldier s = collision.gameObject.GetComponent<Soldier>();
@@ -39,7 +37,5 @@ public class LeftBullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
 
 }
